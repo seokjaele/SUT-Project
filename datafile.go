3937d6796e4567900bcbf728215567698b92e2d8
seokjaele <seokjae1234@naver.com>
2021-09-05 07:45:15 +0900

    test go datafile

---
+++ datafile.go
@@ -0,0 +1,81 @@
+package datafile
+
+import (
+	"bufio"
+	"errors"
+	"os"
+)
+
+type Date struct {
+	year  int
+	month int
+	day   int
+}
+
+type Event struct {
+	Title string
+	Date
+}
+
+func (d *Date) SetYear(year int) error {
+
+	if year < 1 {
+		return errors.New("invalid")
+	}
+
+	d.year = year
+
+	return nil
+}
+
+func (d *Date) SetMonth(month int) error {
+
+	if month < 1 || month > 12 {
+		return errors.New("invalid")
+	}
+
+	d.month = month
+
+	return nil
+}
+
+func (d *Date) SetDay(day int) error {
+
+	if day < 1 || day > 31 {
+		return errors.New("invalid")
+	}
+
+	d.day = day
+
+	return nil
+}
+
+func GetStrings(filename string) ([]string, error) {
+
+	var lines []string
+
+	file, err := os.Open(filename)
+
+	if err != nil {
+		return nil, err
+	}
+
+	scanner := bufio.NewScanner(file)
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		lines = append(lines, line)
+	}
+
+	err = file.Close()
+
+	if err != nil {
+		return nil, err
+	}
+
+	if scanner.Err() != nil {
+		return nil, scanner.Err()
+	}
+
+	return lines, nil
+}
